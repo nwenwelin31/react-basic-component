32c68c590238dbcfe3f38fb86c7cd5e82fa99b16
Nwe Nwe Lin <nwenwelinn@gicjp.com>
2024-02-02 13:52:26 +0630

    Fetch data from API in React with Axios

--- src/App.js
+++ src/App.js
@@ -1,12 +1,45 @@
-import logo from './react-logo.png';
+
 import './App.css';
-import Block2 from './Block2';
-import Block3 from './Block3';
-import Block1 from './Block1';
+import Block2 from './Block/Block2';
+import Block3 from './Block/Block3';
+import Block1 from './Block/Block1';
+import { useEffect, useState } from 'react';
+import axios from 'axios';
 
 function App() {
+  //fetch data from api
+  const [data,setData]=useState([])
+  useEffect(()=>{
+    axios.get('http://127.0.0.1:8000/api/category')
+    .then(res=>setData(res.data))
+    .catch(err=>console.log(err));
+  },[])
   return (
     <div className="App">
+      <div className='container'>
+        <div className='card m-5'>
+          <h3>Fetch data from API in React with Axios</h3>
+          <table className='table'>
+              <thead>
+                <tr>
+                  <th>ID</th>
+                  <th>Name</th>
+                </tr>
+              </thead>
+              <tbody>
+                {/* api data display on react */}
+                {
+                data.map((category,index) => {
+                    return  <tr key={index}>
+                        <td>{category.id}</td>
+                        <td>{category.name}</td>
+                    </tr>
+                })
+                }
+              </tbody>
+          </table>
+        </div>
+      </div>
       <Block1></Block1>
       <Block2></Block2>
       <Block3></Block3>
